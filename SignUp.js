81d1324fdd77ed920a5f4c909361e8292a45d1a3
bunbun <71453005+AmmaraSirkhot@users.noreply.github.com>
2021-08-19 13:39:15 +0530

    signUp-DailyUI001

---
+++ SignUp.js
@@ -0,0 +1,19 @@
+$(".signInbtn, .create-btn").click(function(){
+  window.location.href="otherhtml/signInpg.html";
+});
+
+$(".signUpbtn").click(function(){
+  window.location.href="otherhtml/createacc.html";
+});
+
+$(".backarrow").click(function(){
+  window.location.href="home.html";
+});
+
+$(".twitter-btn").click(function(){
+  window.location.href="https://twitter.com/";
+});
+
+$(".google-btn").click(function(){
+  window.location.href="https://www.google.com/";
+});
